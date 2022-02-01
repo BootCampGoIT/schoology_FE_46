4596688824f55e7506e32d8d45f58b102d72f081
AndriiShnurkov <Shans12.84@ukr.net>
2022-02-01 21:43:41 +0200

    added dom

--- src/index.js
+++ src/index.js
@@ -1,62 +1,99 @@
-import commonServices from "./common/common.js";
-import schoology from "./data/data.js";
-import coursesServices from "./modules/courses.js";
-
-const html = {
-  name: "HTML_CSS",
-};
-
-const js = {
-  name: "Java script",
-};
-
-const group = {
-  name: "FE_46",
-  studentsCount: 20,
-};
-
-// commonServices.renameItem("courses", "4568787", "https: //ghjhjjhjkhkjhjk");
-// // commonServices.addItem("groups", group);
-// // console.log(commonServices.getFieldItems("groups"));
-// // commonServices.addItem("courses", html);
-// console.log(commonServices.getFieldItems("courses"));
-
-const str = "courses.materials.video";
-
-// const res = str.split("."); // ["courses", "dfghkj", "cfghj"];
-
-// =================================================
-
-// const method = (path, id, value) => {
-//   // schoology.courses.materials.video ="https://fghg"
-//   const arr = path.split(".");
-//   const length = arr.length;
-
-//   const res = arr.reduce((acc, item) => {
-//     // console.log(item);
-//     // console.log(schoology[item]);
-//     acc = schoology[item];
-//     return acc;
-//   }, schoology);
+// const js = {
+//   body: {
+//     children: {
+//       h1: {
+//         textContent: "Hello",
+//         styles: ["color: red"],
+//       },
+//       ul: {
+//         classList: [],
+//         children: []
+//           li1: {
+//             children: {
+//               a: {
+//                 textContent: 11111111111,
+//                 href: "https://"
+//               },
+//             },
+//           },
+//           li2: {},
+//         },
+//       },
+//     },
+//   },
 // };
 
-// method(str, "https://fghg");
+// const list = {
+//   id: ""
+// }
 
-const arr = [
-  [1, 2, 3],
-  [4, [111, 22222, 344, 5556], 6],
-  [7, 8, [9, 10, 11, [12, 13, 14]]],
-];
+// const list = document.querySelector(".list");
 
-const getValue = (arr) => {
-  const getArrValue = (value) => {
-    for (let i = 0; i < value.length; i += 1) {
-      if (value[i].constructor.name === "Array") {
-        getArrValue(value[i]);
-      } else console.log(value[i]);
-    }
-  };
-  getArrValue(arr);
+// const listElements = list.querySelectorAll("li");
+
+// listElements.forEach((li) => li.classList.add("test"));
+
+// const courseForm = document.querySelector('[name="courseForm"]');
+// console.log(courseForm);
+
+// console.dir(document);
+// const courseForm = document.forms.courseForm;
+// console.dir(courseForm);
+
+// console.dir(courseForm.submitButton);
+// courseForm.clearButton.textContent = "Clear form";
+
+// const courseForm = document.forms.courseForm;
+// courseForm.courseName.value = "Hello";
+
+// courseForm.term.checked = false;
+
+// // const pict = document.querySelector(".img");
+// // confirm("OK");
+// // pict.src = path;
+
+// console.log(courseForm.classList.toggle("testClass"));
+// console.log(courseForm.classList.toggle("testClass"));
+
+// const h1 = document.createElement("h1");
+// h1.textContent = "Hello my element";
+
+// document.querySelector("body").prepend(h1);
+
+// document.forms.courseForm.courseName.before(h1);
+
+// const list = document.querySelector("ul");
+
+// const fragment = document.createDocumentFragment();
+
+// for (let i = 1; i <= 4; i += 1) {
+//   const listItem = document.createElement("li");
+//   listItem.textContent = i;
+//   fragment.appendChild(listItem);
+// }
+
+// list.appendChild(fragment);
+
+// ===================
+// document.createDocumentFragment
+
+// ===========================
+
+const arr = [1, 2, 3, 4, 5, 6];
+
+const listItemMarkup = (content) => `<li class="fgjhk">${content}</li>`;
+
+const listMurkup = (arr) => {
+  return arr.reduce((acc, item) => {
+    acc += listItemMarkup(item);
+    return acc;
+  }, "");
 };
-getValue(arr);
 
+const markup = listMurkup(arr);
+console.log(markup);
+
+const list = document.querySelector(".pagination");
+list.innerHTML = markup;
+
+list.insertAdjacentHTML("afterbegin", listItemMarkup(7));
--- src/modules/courses.js
+++ src/modules/courses.js
@@ -1,29 +1,13 @@
 import schoology from "../data/data.js";
 
-
-
-
-const renameCourse = (newName, id) => {
-  //('html', '4568787')
-  schoology.courses = schoology.courses.map((course) =>
-    course.id === id ? { ...course, name: newName } : course
-  );
-};
-
-
-
 const addCourseVideo = (id, video) => {
   schoology.courses = schoology.courses.map((course) =>
     course.id === id ? { ...course, materials: { video } } : course
   );
 };
 
-
-
 const coursesServices = {
-
-  renameCourse,
   addCourseVideo,
 };
 export default coursesServices;
-// export { addCourse, removeCourse, renameCourse };
+
